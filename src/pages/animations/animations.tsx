1d72954e6659ff26ce8bfc959de49a8386d30458
Arthur Emanuilov <111119044+PlayGanster@users.noreply.github.com>
2024-10-07 22:08:45 +0300

    Add files via upload

--- src/pages/animations/animations.tsx
+++ src/pages/animations/animations.tsx
@@ -1,9 +1,11 @@
+import AnimationsContent from "@/components/pages/animations/animations-content"
 import AnimationsHeader from "@/components/pages/animations/animations-header"
 
 const Animations = () => {
   return (
     <div className="animations-page">
         <AnimationsHeader />
+        <AnimationsContent />
     </div>
   )
 }
