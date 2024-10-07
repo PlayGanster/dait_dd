5116a8ea35ea8d28f28d58cd693e166871937497
Arthur Emanuilov <111119044+PlayGanster@users.noreply.github.com>
2024-10-07 21:56:44 +0300

    Add files via upload

--- src/app/router/router.tsx
+++ src/app/router/router.tsx
@@ -1,29 +1,29 @@
 import { RouterList } from "@/data/router/router-data"
 import { RouterType } from "@/types/types"
-import { Suspense, useEffect, useState } from "react"
+import { Suspense, useEffect } from "react"
 import { Route, Routes } from "react-router-dom"
-import LoaderDefault from "../loader-default/loader-default"
+// import LoaderDefault from "../loader-default/loader-default"
 
 const telegram = window.Telegram.WebApp
 
 const Router = () => {
-    const [isLoading, setIsLoading] = useState(true);
+    // const [isLoading, setIsLoading] = useState(true);
 
-    const handleLoading = () => {
-        setIsLoading(false);
-    }
+    // const handleLoading = () => {
+    //     setIsLoading(false);
+    // }
 
-    useEffect(()=>{
-        window.addEventListener("load",handleLoading);
-        return () => window.removeEventListener("load",handleLoading);
-    },[])
+    // useEffect(()=>{
+    //     window.addEventListener("load",handleLoading);
+    //     return () => window.removeEventListener("load",handleLoading);
+    // },[])
 
     useEffect(() => {
         telegram.ready()
         telegram.headerColor = "#0D1117"
     }, [])
-
-    return !isLoading ? (
+    // !isLoading ?
+    return (
     <Routes>
         {
             RouterList.map((el: RouterType, index: number) => (
@@ -31,9 +31,10 @@ const Router = () => {
             ))
         }
     </Routes>
-    ):(
-        <LoaderDefault />
     )
+    // :(
+    //     <LoaderDefault />
+    // )
 }
 
 export default Router
