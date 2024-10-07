b4e99b92fe6c13f2bd0265ae71d622b7b6e1a2b2
Arthur Emanuilov <111119044+PlayGanster@users.noreply.github.com>
2024-10-07 22:28:47 +0300

    Add files via upload

--- src/components/pages/politics/politics-content.tsx
+++ src/components/pages/politics/politics-content.tsx
@@ -1,13 +1,19 @@
 import "@styles/pages/politics/politics-content.scss"
+import { useNavigate } from "react-router-dom"
+
+const telegram = window.Telegram.WebApp
 
 const PoliticsContent = () => {
-  return (
-    <div className="politics-content">
-        <p className="content__text">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi neque nisi facilis ratione mollitia, eum nam cumque. Dignissimos ea quisquam sed nam aspernatur similique ipsam optio qui quos suscipit placeat ex dolor corrupti ratione aliquid excepturi hic, alias consequatur sit culpa! Dicta nobis consequatur recusandae corporis in ut! Dolor quae itaque perspiciatis deleniti ullam recusandae architecto.
-        </p>
-    </div>
-  )
+    const navigate = useNavigate();
+    telegram.BackButton.onClick(() => navigate("/settings"))
+
+    return (
+        <div className="politics-content">
+            <p className="content__text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi neque nisi facilis ratione mollitia, eum nam cumque. Dignissimos ea quisquam sed nam aspernatur similique ipsam optio qui quos suscipit placeat ex dolor corrupti ratione aliquid excepturi hic, alias consequatur sit culpa! Dicta nobis consequatur recusandae corporis in ut! Dolor quae itaque perspiciatis deleniti ullam recusandae architecto.
+            </p>
+        </div>
+    )
 }
 
 export default PoliticsContent
