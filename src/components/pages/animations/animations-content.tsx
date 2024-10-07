acdb615943477a193fbaa167988b078c8e9f13e1
Arthur Emanuilov <111119044+PlayGanster@users.noreply.github.com>
2024-10-07 22:08:22 +0300

    Add files via upload

---
+++ src/components/pages/animations/animations-content.tsx
@@ -0,0 +1,23 @@
+import "@styles/pages/animations/animations-content.scss"
+import { useEffect } from "react"
+import { useNavigate } from "react-router-dom"
+
+const telegram = window.Telegram.WebApp
+
+const AnimationsContent = () => {
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        telegram.BackButton.show()
+    }, [])
+
+    telegram.BackButton.onClick(() => navigate("/"))
+
+    return (
+        <div className="animations-content">
+
+        </div>
+    )
+}
+
+export default AnimationsContent
